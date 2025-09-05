c15762b11bbc443d3d622090cf057a65e17057a7
Andres Pedraza <andrespedca@gmail.com>
2025-09-05 15:39:24 -0500

    completed stats assignemt

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    file_string = get_book_text("./books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    file_string = get_book_text("books/frankenstein.txt")
+    print("Analyzing book found at books/frankenstein.txt...")
     num_words = get_num_words(file_string)
-    print(f"{num_words} words found in the document")
-    print(sort_dictionary(words_frequency(file_string)))
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    list_dictionary_sorted = sort_dictionary(words_frequency(file_string))
+    for d in list_dictionary_sorted:
+        ch, val = d.items()
+        print(f"{ch[1]}: {val[1]}")
+    print("============= END ===============")
 
 main()
